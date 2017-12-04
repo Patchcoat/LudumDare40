574f5ca6b86e1a258274f72fcdcf16886b74f8f3
Patchcoat <spiencewelch@gmail.com>
2017-12-04 00:16:38 -0800

    Added Spy Satalite

---
+++ Ludum Dare 40/Assets/Scripts/Follower.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Follower : MonoBehaviour {
+
+    [SerializeField] GameObject ObjectToFollow;
+
+    private Vector3 newPosition;
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        newPosition = new Vector3(ObjectToFollow.transform.position.x, transform.position.y, ObjectToFollow.transform.position.z);
+        transform.position = newPosition;
+    }
+}
